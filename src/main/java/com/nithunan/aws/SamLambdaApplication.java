9fc209694c1189b46d1763f071cab98ea4c95d8d
nithyanandhamv <nithyanandhamv@pdisoftware.com>
2021-12-09 10:43:07 +0530

    Add sam template.yaml

--- src/main/java/com/nithunan/aws/SamLambdaApplication.java
+++ src/main/java/com/nithunan/aws/SamLambdaApplication.java
@@ -1,13 +1,29 @@
 package com.nithunan.aws;
 
+import java.time.LocalDateTime;
+import java.util.function.Function;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @SpringBootApplication
 public class SamLambdaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SamLambdaApplication.class, args);
+	}
+	
+	@Bean
+	public Function<String, Void> schedule() {
+		return (input) -> {
+			log.info("Triggered lambda at " + LocalDateTime.now());
+			log.info("Received Message: " + input);			
+			return null;
+		};
 	}
 
 }
