babbd5807c76a031cdf2e56987316838eba9fbd1
nithyanandhamv <nithyanandhamv@pdisoftware.com>
2021-12-09 09:16:07 +0530

    Initial commit

---
+++ src/main/java/com/nithunan/aws/SamLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.nithunan.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamLambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nithunan/aws/SamLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nithunan.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
